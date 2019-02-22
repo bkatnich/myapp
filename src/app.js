663bc50aee2569834b36fa35dbbe3546c7e7c2e6
Britton Katnich <katnich@shaw.ca>
2019-02-21 23:53:28 -0800

    third commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     res.send('Hello world to my bitching app!');
 });
 
-app.listen(port, () =>
+app.listen(3000, () =>
   console.log('Example app listening on port:' + port),
 );
 
